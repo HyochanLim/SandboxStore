9a813419d75144c205f7260436efc3597a08a5e2
Ivan Lim <hyochan5211@snu.ac.kr>
2026-02-25 18:29:31 +0900

    add error handling function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const csrf = require("csurf");
 const db = require("./data/database");
 const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
 
 const authRoutes = require("./routes/auth.routes");
 
@@ -17,7 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(csrf());
 
+app.use(addCsrfTokenMiddleware);
+
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
