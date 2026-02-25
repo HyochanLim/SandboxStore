4175252420737aae2bdc5812401ff3d6624904b9
Ivan Lim <hyochan5211@snu.ac.kr>
2026-02-25 17:36:26 +0900

    create structure, signup feature

---
+++ controllers/auth.controller.js
@@ -0,0 +1,34 @@
+const User = require("../models/user.model");
+
+function getSignup(req, res) {
+    res.render('auth/signup', {
+        pageTitle: 'Signup',
+    });
+}
+
+async function signup(req, res) {
+    const user = new User(
+        req.body.email,
+        req.body.password,
+        req.body.fullname,
+        req.body.street,
+        req.body.postal,
+        req.body.city
+    );
+    
+
+    user.signup();
+    res.redirect("/login");
+}
+
+function getLogin(req, res) {
+    res.render('auth/login', {
+        pageTitle: 'Login',
+    });
+}
+
+module.exports = {
+  getSignup: getSignup,
+  getLogin: getLogin,
+  signup: signup,
+};
